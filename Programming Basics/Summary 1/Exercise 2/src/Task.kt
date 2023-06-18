ef00e75fca98795e1cd8c854a4ef0ca2dbd02a87
joohwan9607 <joohwan9607@mobility42.io>
2023-06-19 02:16:30 +0900

    test

--- Programming Basics/Summary 1/Exercise 2/src/Task.kt
+++ Programming Basics/Summary 1/Exercise 2/src/Task.kt
@@ -3,6 +3,12 @@ package summaryIExercise2
 
 fun other(s: String): String {
   TODO()
+  val result = ""
+  val size = s.length
+  for(i in 1..size step 2){
+    result += s[i-1]
+  }
+  println(result)
 }
 
 fun main() {
